be8a6768f43e668938043758d15366d1867ce8e1
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-21 19:39:44 -0300

    Player Walking Animation

--- Clothes Shop/Assets/Scripts/Player/PlayerController.cs
+++ Clothes Shop/Assets/Scripts/Player/PlayerController.cs
@@ -9,10 +9,20 @@ public class PlayerController : MonoBehaviour
     public Player playerData;
     public FloatVariable walkingSpeed;
 
+    private Animator playerAnimator;
     private Rigidbody2D playerRigidbody;
+
+    #region Animator Parameters
+
+    [Header("Animator Parameters")]
+    public string horizontalMovementParameterName;
+    public string verticalMovementParameterName;
+
+    #endregion
 
     private void Awake()
     {
+        playerAnimator = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -25,7 +35,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
@@ -33,9 +43,77 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
     }
 
+    /// <summary>
+    /// Returns a unitary vector in a 4-way movement style, depending on the user input. Does not allow more than one key to be pressed at the same time.
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 Get4WayPlayerMovement()
+    {
+        Vector2 returnValue;
+        float horizontalInputValue = Input.GetAxisRaw("Horizontal");
+        float verticalInputValue = Input.GetAxisRaw("Vertical");
+
+        //The following code will make it so that only 4-axis movements are valid.
+        if (horizontalInputValue > 0)
+        {
+            if (verticalInputValue == 0)
+            {
+                returnValue = Vector2.right;
+            }
+            else
+            {
+                returnValue = Vector2.zero;
+            }
+        }
+        else if (horizontalInputValue < 0)
+        {
+            if (verticalInputValue == 0)
+            {
+                returnValue = Vector2.left;
+            }
+            else
+            {
+                returnValue = Vector2.zero;
+            }
+        }
+        else if (verticalInputValue > 0)
+        {
+            if (horizontalInputValue == 0)
+            {
+                returnValue = Vector2.up;
+            }
+            else
+            {
+                returnValue = Vector2.zero;
+            }
+        }
+        else if (verticalInputValue < 0)
+        {
+            if (horizontalInputValue == 0)
+            {
+                returnValue = Vector2.down;
+            }
+            else
+            {
+                returnValue = Vector2.zero;
+            }
+        }
+        else
+        {
+            returnValue = Vector2.zero;
+        }
+        return returnValue;
+    }
+
     private void PlayerMovement()
     {
-        Vector2 movementAxesValues = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 movementAxesValues = Get4WayPlayerMovement();
+     
+        //Sets the player's rigidbody's velocity
         playerRigidbody.velocity = new Vector2(movementAxesValues.x * walkingSpeed.Value, movementAxesValues.y * walkingSpeed.Value) * Time.fixedDeltaTime;
+
+        //Update animator parameters
+        playerAnimator.SetFloat(horizontalMovementParameterName, movementAxesValues.x);
+        playerAnimator.SetFloat(verticalMovementParameterName, movementAxesValues.y);
     }
 }
