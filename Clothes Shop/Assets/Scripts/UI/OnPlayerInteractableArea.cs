3b615499f278f26f468312db6a55ef786a0a71e6
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-22 14:05:37 -0300

    OnPlayerInteractableArea
    Used to trigger events when the player is nearby

---
+++ Clothes Shop/Assets/Scripts/UI/OnPlayerInteractableArea.cs
@@ -0,0 +1,36 @@
+﻿using ClothesShop.Mechanics.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ClothesShop.Mechanics
+{
+    public class OnPlayerInteractableArea : MonoBehaviour
+    {
+        [Serializable]
+        public class OnPlayerEnterInteractableArea : UnityEvent<Interactable> { }
+        [Serializable]
+        public class OnPlayerExitInteractableArea : UnityEvent<Interactable> { }
+
+        public OnPlayerEnterInteractableArea onPlayerEnterInteractableArea;
+        public OnPlayerExitInteractableArea onPlayerExitInteractableArea;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                onPlayerEnterInteractableArea?.Invoke(collision.gameObject.GetComponent<Interactable>());
+            }
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                onPlayerExitInteractableArea?.Invoke(collision.gameObject.GetComponent<Interactable>());
+            }
+        }
+    }
+}
+
