6fbbc5490153e17e77efe9090f311f14c2c26b55
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-22 04:42:59 -0300

    Basic Player Money UI

---
+++ Clothes Shop/Assets/Scripts/UI/IntSOToTextMeshProUGUIText.cs
@@ -0,0 +1,28 @@
+﻿using ScriptableObjectExtensions.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ClothesShop.UI
+{
+    public class IntSOToTextMeshProUGUIText : MonoBehaviour
+    {
+        [Header("Prefix and Suffix")]
+        [SerializeField] private string prefixString;
+        [SerializeField] private string suffixString;
+        private TextMeshProUGUI textMeshProUGUI;
+        [Header("IntVariable")]
+        [SerializeField] private IntVariable intVariable;
+
+        private void Awake()
+        {
+            textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void OnEnable()
+        {
+            textMeshProUGUI.text = string.Format("{0}{1}{2}", prefixString, intVariable.Value.ToString(), suffixString);
+        }
+    }
+}
