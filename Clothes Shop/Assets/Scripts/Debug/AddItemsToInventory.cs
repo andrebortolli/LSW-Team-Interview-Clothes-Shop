d1f9efe618dda87e6cd1091c29965c4d15d10e30
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-23 00:11:37 -0300

    46h Build

---
+++ Clothes Shop/Assets/Scripts/Debug/AddItemsToInventory.cs
@@ -0,0 +1,33 @@
+﻿using ClothesShop.SO.Inventory;
+using ClothesShop.SO.Item;
+using ClothesShop.SO.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddItemsToInventory : MonoBehaviour
+{
+    public Inventory allItemsInventory;
+    public Player[] playersToAddItems;
+
+    public void AddItemToInventories(int _amountOfEach)
+    {
+        if (playersToAddItems.Length > 0)
+        {
+            foreach(Item item in allItemsInventory.Items)
+            {
+                for(int i = 0; i < _amountOfEach; i++)
+                {
+                    foreach(Player player in playersToAddItems)
+                    {
+                        player.inventory.AddItem(item);
+                    }
+                }
+            }
+            foreach(Player player in playersToAddItems)
+            {
+                player.wallet.Value += 10000;
+            }
+        }
+    }
+}
