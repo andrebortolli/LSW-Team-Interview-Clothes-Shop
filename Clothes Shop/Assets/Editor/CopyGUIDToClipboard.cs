a977537178b77ca98fb5a12c3ad5dd5079cf99fa
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-24 16:23:39 -0300

    Outfit 3 Template

---
+++ Clothes Shop/Assets/Editor/CopyGUIDToClipboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CopyGUIDToClipboard : EditorWindow
+{
+
+    [MenuItem("Assets/Copy Selected Item GUID to Clipboard", false, 0)]
+    public static void GUIDToClipboard()
+    {
+        string t = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(Selection.activeObject));
+        GUIUtility.systemCopyBuffer = t;
+        Debug.Log("Current Selection GUID: " + t);
+    }
+}
