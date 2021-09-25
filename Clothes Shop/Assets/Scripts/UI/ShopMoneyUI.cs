1c46768a642b9ba494b506a8828cb6c94249be17
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2021-09-25 03:44:25 -0300

    Build 85h - Improved shop wallets; Organized scripts; Reset Inventory

---
+++ Clothes Shop/Assets/Scripts/UI/ShopMoneyUI.cs
@@ -0,0 +1,29 @@
+﻿using ScriptableObjectExtensions.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ClothesShop.UI
+{
+    public class ShopMoneyUI : MonoBehaviour
+    {
+        [Header("Variables")]
+        [SerializeField] private StringVariable playerName;
+        [SerializeField] private IntVariable wallet;
+        private TextMeshProUGUI textMeshProUGUI;
+
+        private void Awake()
+        {
+            textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            if (playerName != null && wallet != null)
+            {
+                textMeshProUGUI.text = string.Format("{0}: ${1}", playerName.Value, wallet.Value.ToString());
+            }
+        }
+    }
+}
